14ec0c5c95bc32db2fa4aec76f64de125cc5d74f
acs <acs@localhost>
2007-04-13 01:00:30 +0000

    Add aliases for office executables.

--- binfiles/NT/cygwin/cygwin_update.cmd
+++ binfiles/NT/cygwin/cygwin_update.cmd
@@ -24,4 +24,9 @@ if X%CYGWIN_SITE% == X set CYGWIN_SITE="rsync://mirrors.kernel.org/sourceware/cy
 
 rsync -vaz --exclude mail-archives %CYGWIN_SITE% .
 
-.\setup --local-install --local-package-dir f:/cygwin/cygwin_repo --no-shortcuts
+rem I tried to use cygwin setup based on desktop shortcut, not default setup in repo,
+rem but that caused the rsync window to disappear, so instead, I specify
+rem my locally-built latest copy of setup from /usr/local/bin.
+rem .\setup --local-install --local-package-dir f:/cygwin/cygwin_repo --no-shortcuts
+rem "%HOMEDRIVE%%HOMEPATH%\Desktop\Cygwin Setup.lnk"
+e:\cygwin\usr\local\bin\cygwin-setup --local-install --local-package-dir f:/cygwin/cygwin_repo --no-shortcuts

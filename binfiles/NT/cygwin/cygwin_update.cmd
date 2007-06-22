82589235efd915ffe30b3abcfce0ceaa68714294
acs <55901226+vshelton@users.noreply.github.com>
2007-06-22 01:04:40 +0000

    Break up cygwin update script

--- binfiles/NT/cygwin/cygwin_update.cmd
+++ binfiles/NT/cygwin/cygwin_update.cmd
@@ -1,26 +1,12 @@
 @echo off
 
-rem How to fetch rsync list:
-rem wget -O - http://cygwin.com/mirrors.txt | grep rsync | cut -d';' -f1 | sed -e 's;\(rsync://[0-9a-z.-]*\).*;\1;'
+zsh -c "echo -n set CYGWIN_SITE=;/e/acs/bin/cygwin_mirrors" > x.bat
 
-rem The current list:
-rem rsync://rsync.osuosl.org
-rem rsync://sigunix.cwru.edu
-rem rsync://mirrors.xmission.com
-rem rsync://mirrors.kernel.org
-rem rsync://rsync.gtlib.gatech.edu
-rem rsync://mirror.etf.bg.ac.yu
-rem rsync://ftp.kaist.ac.kr
-rem rsync://mirror.averse.net
-rem rsync://ftp.esat.net
-rem rsync://bo.mirror.garr.it
-rem rsync://ftp.gwdg.de
-rem rsync://ftp.inf.tu-dresden.de
+call x.bat
+del /f x.bat
 
 f:
 cd \cygwin\cygwin_repo
-
-if X%CYGWIN_SITE% == X set CYGWIN_SITE="rsync://mirrors.kernel.org/sourceware/cygwin/"
 
 rsync -vaz --exclude mail-archives %CYGWIN_SITE% .
 

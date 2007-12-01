cb05345f06a37b237726eb7cbc3ae2c447b40167
acs <55901226+vshelton@users.noreply.github.com>
2007-12-01 03:46:54 +0000

    patch info fonts on windoze

--- rcfiles/lib/lisp/xemacs/default.el
+++ rcfiles/lib/lisp/xemacs/default.el
@@ -149,6 +149,7 @@
       (set-face-font 'default (format "%s:%s:%d::%s"
 				      family "Regular" size encoding))
       (set-face-background 'default "wheat")
+      (setq Info-startup-hook 'acs::set-info-fonts)
       ;; These resets are currently required if we're not using 10 point.
       (reset-face 'bold)
       (reset-face 'italic)
@@ -172,6 +173,12 @@
 (setq tag-table-alist
       '(("\\.emacs$" . (xemacs-source-directory))
 	("/lisp" . (xemacs-source-directory))))
+
+(defun acs::set-info-fonts ()
+  "Remove the crappy italic font from info-nodes"
+  (interactive)
+  (make-face-unitalic 'info-node)
+  (set-face-foreground 'info-node "blue"))
 
 (defun xemacs-source-directory ()
   "Return the path to the root of the XEmacs source tree."

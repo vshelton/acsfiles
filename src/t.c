00a66c8d1abef6edf3fcc6188dbd426774d6c9c0
Vin Shelton <55901226+vshelton@users.noreply.github.com>
2012-07-31 14:27:18 -0400

    Added debug_array()

--- src/t.c
+++ src/t.c
@@ -6,13 +6,17 @@
 
 #define N_ELEMS(array)          (sizeof(array) / sizeof(array[0]))
 
-#define debug_var(v, fmt)       printf(#v " = %" #fmt "\n", v)
-#define debug_var2(v, f1, f2)   printf(#v " = %" #f1 " (%" #f2 ")\n", v, v)
+#define debug_array(v)                                                  \
+    do {                                                                \
+        for ( int i = 0; i < N_ELEMS(v); ++i ) {                        \
+            printf(#v "[%d] (%p) = %d (%#x)\n", i, &v[i], v[i], v[i]);  \
+        }                                                               \
+    } while (0)
 #define debug_ptr(p, fmt)       printf(#p " = %p; *" #p " = %" #fmt "\n", p, *p)
 #define debug_ptr2(p, f1, f2)   printf(#p " = %p; *" #p " = %" #f1 " (%" #f2 ")\n", p, *p, *p)
-
 #define debug_sizeof(typ)       printf("sizeof(" #typ ") = %lu\n", (unsigned long)sizeof(typ))
-
+#define debug_var(v, fmt)       printf(#v " = %" #fmt "\n", v)
+#define debug_var2(v, f1, f2)   printf(#v " = %" #f1 " (%" #f2 ")\n", v, v)
 
 typedef int foo;
 foo bar = 2;
@@ -45,25 +49,19 @@ main()
     zap.b = 2;
     char *p = "Test";
     while ( *p ) {
-        debug_ptr(p, c);
+        debug_ptr2(p, c, #x);
         ++p;
     }
 
-    short sarr[] = { 1, 2, 3 };
-    int i;
-    for ( short *sp = sarr, i = N_ELEMS(sarr); i > 0; ++sp, --i ) {
-        debug_ptr2(sp, d, #x);
-    }
+    short sarr[] = { 101, 202, 303 };
+    debug_array(sarr);
 
     int iarr[2];
-    iarr[0] = 4;
-    iarr[1] = 5;
-    iarr[2] = 6;            // Runs off the end of the array.
+    iarr[0] = 404;
+    iarr[1] = 505;
+    iarr[2] = 606;          // Runs off the end of the array.
                             // Detected by clang, not by gcc.
-
-    for ( int *ip = iarr, i = N_ELEMS(iarr); i > 0; ++ip, --i ) {
-        debug_ptr2(ip, d, #x);
-    }
+    debug_array(iarr);
 
 #if defined(PATH_MAX)
     debug_var2(PATH_MAX, d, #x);
@@ -90,3 +88,8 @@ main()
 
     return pwd();
 }
+
+// Local Variables:
+// c-basic-offset: 4
+// indent-tabs-mode: nil
+// End:

ef07c94dab0ae0f621f594e2bfe47daaeb8b0bae
Vin Shelton <acs@xemacs.org>
2014-01-26 13:33:28 -0500

    New acs-custom

--- rcfiles/lib/lisp/fsf/acs-custom.el
+++ rcfiles/lib/lisp/fsf/acs-custom.el
@@ -1,32 +1,21 @@
-;;(load "eshell-auto")
-
 ;; Set these variables via custom
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(*try-oblique-before-italic-fonts* t)
  '(auto-save-timeout 960)
  '(balloon-help-background "LightYellow")
- '(bbdb-use-pop-up nil)
  '(c-hanging-comment-ender-p nil)
  '(c-indent-comments-syntactically-p t)
  '(c-macro-preprocessor "gcc -C -E -")
  '(c-macro-prompt-flag t)
- '(canlock-password "bc13bbe494dc254b3e5049a0ca3afa3139395fff")
  '(column-number-mode t)
  '(compilation-error-regexp-systems-list (quote (gnu comma)))
  '(compilation-mouse-motion-initiate-parsing t)
- '(compilation-scroll-output t)
- '(delete-selection-mode nil)
- '(desktop-save-mode t)
  '(enable-local-eval t)
- '(enable-local-variables :safe)
- '(eshell-modules-list (quote (eshell-alias eshell-banner eshell-basic eshell-cmpl eshell-dirs eshell-glob eshell-hist eshell-ls eshell-pred eshell-prompt eshell-rebind eshell-script eshell-term eshell-unix)))
  '(explicit-shell-file-name shell-file-name)
  '(find-file-compare-truenames t)
- '(find-file-visit-truename t)
  '(gnus-expert-user t)
  '(gnus-novice-user nil)
  '(gnus-suppress-duplicates t)
@@ -34,18 +23,16 @@
  '(gnuserv-frame (quote gnuserv-visible-frame-function))
  '(indent-tabs-mode t)
  '(inhibit-startup-screen t)
+ '(line-number-mode t)
  '(mail-user-agent (quote message-user-agent))
- '(mark-even-if-inactive t)
  '(modeline-click-swaps-buffers t)
  '(mouse-yank-at-point nil)
  '(next-line-add-newlines nil)
  '(query-replace-highlight t)
  '(query-user-mail-address nil)
- '(recentf-mode t)
- '(require-final-newline nil)
- '(resize-minibuffer-frame-exactly t)
- '(scroll-bar-mode (quote right))
- '(send-mail-function (quote sendmail-send-it))
+ '(require-final-newline t)
+ '(resize-minibuffer-frame-exactly t t)
+ '(shell-file-name "zsh")
  '(shell-multiple-shells t)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
@@ -57,5 +44,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:foreground "DodgerBlue1"))))
- '(menu ((t (:weight bold :height 3.0 :family "lucida")))))
+ '(font-lock-comment-face ((t (:foreground "DodgerBlue1")))))

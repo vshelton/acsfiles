a7c4ba10d5d64b1d8f5f9c5d37ce804758d0ffbf
Vin Shelton <acs@xemacs.org>
2014-11-26 09:32:30 -0500

    Add snapshot_directory for backups

--- src/base32_test/base32_test.c
+++ src/base32_test/base32_test.c
@@ -11,9 +11,32 @@ checkbase32(char testchar)
 }
 
 int
+print_variable_sizes(void)
+{
+#if defined(__WORDSIZE)
+    printf("Word size = %d\n", __WORDSIZE);
+#endif
+
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ - 0 >= 199901L)
+# define SIZEOF_FORMAT	"zu"
+#else
+# define SIZEOF_FORMAT	"lu"
+#endif
+
+    printf("sizeof(long) = %" SIZEOF_FORMAT " bytes \n", sizeof(long));
+    printf("sizeof(int) = %" SIZEOF_FORMAT " bytes \n", sizeof(int));
+    printf("sizeof(short) = %" SIZEOF_FORMAT " bytes \n", sizeof(short));
+    printf("sizeof(char) = %" SIZEOF_FORMAT " bytes \n", sizeof(char));
+
+    return 0;
+}
+
+int
 main(void)
 {
     printf("Hello world!\n");
+
+    print_variable_sizes();
 
     checkbase32(10);
     checkbase32('A');

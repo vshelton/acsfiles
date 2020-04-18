e93bb4686998ef5f33ea6fd05ef0feb4a7a0ffea
vshelton <acs@alumni.princeton.edu>
2020-04-18 13:16:00 -0400

    Use a different solarized theme
    Moved stuff around.

--- rcfiles/emacs-init.el
+++ rcfiles/emacs-init.el
@@ -1,40 +1,20 @@
-;; Use ttf-hack as the default font.
-;; Use comic sans ms for the modeline.
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal family "Hack"))))
- '(fixed-pitch ((t (:inherit default))))
- '(font-lock-comment-face ((t (:foreground "DodgerBlue1"))))
- '(mode-line ((t (:background "LightSalmon" :foreground "black" :box (:line-width -1 :style released-button) :height 1.0 :family "comic sans ms"))))
- '(mode-line-buffer-id ((t (:background "LightSalmon" :foreground "black" :weight bold :height 1.0 :family "comic sans ms"))))
- '(mode-line-emphasis ((t (:background "LightSalmon" :foreground "black" :weight bold :height 1.0 :family "comic sans ms"))))
- '(mode-line-highlight ((t (:inherit mode-line))))
- '(mode-line-inactive ((t (:inherit mode-line :inverse-video t)))))
+;;;
+;;; Emacs initialization file
+;;;
+;;; Emacs version >= 24 required!
+;;;
 
 ;; Start up the emacsclient server.
 (server-start)
-
-;;; We require emacs >= 24!
-
-;;; See melpa.org.
-;(require 'package)
-;(add-to-list 'package-archives
-;             '("melpa" . "https://melpa.org/packages/") t)
-;(package-initialize)
-;(package-refresh-contents)
-
-;; Use the solarized theme.
-(add-to-list 'custom-theme-load-path (concat (getenv "SCMROOT") "/emacs-color-theme-solarized"))
-(load-theme 'solarized t)
 
 ;; Save the session for next time.
 (desktop-save-mode 1)
 
 ;; Turn off the tool bar.
 (tool-bar-mode -1)
+
+;; Put the scrollbar on the right.
+(set-scroll-bar-mode 'right)
 
 ;; Show matching parentheses.
 (show-paren-mode 1)
@@ -47,12 +27,41 @@
 ;; Track the column number in the modeline.
 (setq column-number-mode t)
 
-;; Put the scroolbar on the right
-(set-scroll-bar-mode 'right)
+;; Version control customizations.
+(setq vc-follow-symlinks t)
 
-;; Set transparency of emacs
+;; Use active regions.
+(delete-selection-mode t)
+(transient-mark-mode t)
+
+;; Set the transparency of emacs.
 (set-frame-parameter (selected-frame) 'alpha '(92 88))
 (add-to-list 'default-frame-alist '(alpha 92 88))
+
+;; See melpa.org.
+(require 'package)
+;(add-to-list 'package-archives
+;             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+;(package-refresh-contents)
+
+;; Use a solarized color scheme.
+;; This scheme came from https://github.com/bbatsov/solarized-emacs.
+;; The solarized homepage is https://ethanschoonover.com/solarized.
+;(load-theme 'solarized-light t)
+(load-theme 'solarized-dark t)
+
+;; Use Cascadia Code as the default font.
+;; Use comic sans ms for the modeline.
+(custom-set-faces
+ ;; If there is more than one custom-set-faces call, they won't work right.
+ '(default ((t (:family "Cascadia Code" :foundry "microsoft" :height 120))))
+ '(fixed-pitch ((t (:inherit default))))
+ '(font-lock-comment-face ((t (:foreground "DodgerBlue1"))))
+ '(scroll-bar ((t (:foreground "gainsboro" :background "gray2"))))
+ '(mode-line ((t (:background "LightSalmon" :foreground "black" :family "cmic sans ms"))))
+ '(mode-line-inactive ((t (:foreground "LightSalmon" :background "black" :family "comic sans ms"))))
+ '(mode-line-buffer-id ((t (:foreground "black")))))
 
 ;; Local Variables: ***
 ;; End: ***

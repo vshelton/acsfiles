8108d1170526bf900d40e19292b52d3bbe518885
vshelton <acs@alumni.princeton.edu>
2020-05-07 21:04:38 -0400

    Require magit

---
+++ rcfiles/init-vc.el
@@ -0,0 +1,13 @@
+(require 'init-elpa)
+
+(require-package 'magit)
+(require 'magit)
+
+;; Version control customizations.
+(setq vc-follow-symlinks t)
+
+(provide 'init-vc)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:

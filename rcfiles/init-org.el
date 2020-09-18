cd10c6ef337e444e0b0614a8e9720469fda59b05
vshelton <acs@alumni.princeton.edu>
2020-09-18 08:22:23 -0400

    nuc is now hardwired

---
+++ rcfiles/init-org.el
@@ -0,0 +1,11 @@
+
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+
+(provide 'init-org)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
